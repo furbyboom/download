460d45db35d3ccb45b4f78856a710864e13f029a
furbyboom <115577747+furbyboom@users.noreply.github.com>
2023-01-21 12:42:19 -0800

    Update hello world.sh

--- hello world.sh
+++ hello world.sh
@@ -1,1 +1,1 @@
-current=$PWD; cd ~; mkdir furby ;cd furby; touch "hello world"; ls; cd ~; rm -R furby ;cd "$current"; rm "hello%20world.sh"
+current="$PWD"; cd ~; mkdir furby ;cd furby; touch "hello world"; ls; cd ~; rm -R furby ;cd $current; rm "hello%20world.sh"
